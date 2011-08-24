cca03be76a16593df201a6fc26292abf987579af
thatdutchguy <thatdutchguy@secretlymexico.com>
2011-08-24 17:20:28 -0500

    added jasmine specs

---
+++ old/sample2_spec.js
@@ -0,0 +1,24 @@
+// var jasmineEnv = jasmine.getEnv();
+// jasmineEnv.updateInterval = 1000;
+// var rep = new ConsoleReporter();
+// jasmineEnv.addReporter(rep);
+
+
+describe('another test', function() {
+  it('passes', function() {
+    expect(true).toEqual(true)
+  })
+  it('fails', function() {
+    expect(false).toEqual(true)
+  })
+})
+describe('yet another test', function() {
+  it('passes', function() {
+    expect(true).toEqual(true)
+  })
+  it('fails', function() {
+    expect(false).toEqual(true)
+  })
+})
+
+// jasmineEnv.execute();
